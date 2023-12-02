f11aa81fc9425494df0505e962290567dd07b1bd
Alpy81 <alperen.ertas@gmx.de>
2023-12-02 19:23:33 +0100

    update commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
-    "use strict"
-    return Controller.extend("sap.ui.demo.wakthrough.App", {
-        onShowHello : function () {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.App", {
+        onInit: function () {
+            // set the data model on the view
+            var oData = {
+                recipient: {
+                    name: ""
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+        onShowHello: function () {
             // Show a native or Vanilla JS Alert
-           alert("Hello there!");
+            MessageToast.show("Hello there!");
         }
     });
 });
-            
