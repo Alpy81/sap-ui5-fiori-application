47b45da69f1b59e690ea1109259371df0b63ad10
Alpy81 <alperen.ertas@gmx.de>
2023-12-05 18:57:30 +0100

    update commit

--- webapp/component.js
+++ webapp/component.js
@@ -1,40 +1,57 @@
-sap.ui.define(
-  [
+sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel",
-  ],
-  function (UIComponent, JSONModel, ResourceModel) {
+    "./controller/HelloDialog",
+    "sap/ui/Device"
+], function (UIComponent, JSONModel, ResourceModel, HelloDialog, Device) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-      metadata: {
-        rootView: {
-          viewName: "sap.ui.demo.walkthrough.view.App",
-          type: "XML",
-          async: true,
-          id: "app",
+        metadata: {
+            manifest: "json"
         },
-      },
-      init: function () {
-        // call the init function of the parent
-        UIComponent.prototype.init.apply(this, arguments); // "arguments" statt "argument"
+        init: function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            // set data models
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
 
-        let oData = {
-          recipient: {
-            name: "",
-          },
-        };
-        let oModel = new JSONModel(oData);
-        this.setModel(oModel); // "this.getView()" durch "this" ersetzt
+            // set device model
+            var oDeviceModel = new JSONModel(Device);
+            oDeviceModel.setDefaultBindingMode("OneWay");
+            this.setModel(oDeviceModel, "device");
 
-        // set i18n
-        let i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocales: "",
-        });
-        this.setModel(i18nModel, "i18n"); // "this.getView()" durch "this" ersetzt
-      },
+            // set dialog
+            this._helloDialog = new HelloDialog(this.getRootControl());
+
+            // create the views based on the url/hash
+            this.getRouter().initialize();
+        },
+
+        getContentDensityClass : function () {
+            if (!this._sContentDensityClass) {
+                if (!Device.support.touch) {
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                } else {
+                    this._sContentDensityClass = "sapUiSizeCozy";
+                }
+            }
+            return this._sContentDensityClass;
+        },
+
+        exit: function () {
+            this._helloDialog.destroy();
+            delete this._helloDialog;
+        },
+
+        openHelloDialog : function () {
+            this._helloDialog.open();
+        } 
     });
-  }
-);
+});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,24 +1,14 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  function (Controller, MessageToast, JSONModel, ResourceModel) {
-    "use strict";
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-      onShowHello: function () {
-        // read msg from i18n model
-        let oBundle = this.getView().getModel("i18n").getResourceBundle();
-        let sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        onInit: function () {
+            this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+        },
+        onOpenDialog: function () {
+            this.getOwnerComponent().openHelloDialog();
+        }
 
-        // Show message
-        MessageToast.show(sMsg);
-      },
     });
-  }
-);
+});
